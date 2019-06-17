0ea8ffad18989e8cc56d3028ed99d0ebe891ddf2
zuodar <michal.wlodarek@gmail.com>
2019-06-17 17:46:20 +0200

    Next prev buttons loop

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Work from './Work';
 import data from '../data';
+import dataJson from '../data.json';
 
 const centered = {
 	left: '0',
@@ -10,30 +11,41 @@ const centered = {
 
 class Works extends React.Component {
 
-
+d
   constructor(props) {
     super(props);
     this.state = {
-      id:1 
+      id:0 
     };
   }
 
   incrementItem = () => {
-    this.setState({ id: this.state.id + 1 });  
+      if ( this.state.id + 1 < data.length) {
+        this.setState({ id: this.state.id + 1 });  
+      } else {
+        this.setState({ id: 0 }); 
+      }
   };
+
   decrementItem = () => {
-    this.setState({ id: this.state.id - 1 });  
+      if ( this.state.id > 0 ) {
+        this.setState({ id: this.state.id - 1 });  
+      } else {
+        this.setState({ id: data.length - 1 }); 
+      }    
+  
   }
 
 	render() {
     const stateId = this.state.id;
     const url = data[stateId].thumbnail;
     const name = data[stateId].name;
-    const nextUrl = data[stateId + 1].thumbnail;
+
+/*    const nextUrl = data[stateId + 1].thumbnail;
     const nextName = data[stateId + 1].name;
     const prevUrl = data[stateId-1].thumbnail;
     const prevName = data[stateId-1].name; 
-
+*/
     return (
 
 
@@ -41,20 +53,20 @@ class Works extends React.Component {
           <div className="wrapper">
               <div id="slides" className="slides">
 
-                  <div className="slide jag-prev-post ">
+{/*                  <div className="slide jag-prev-post ">
                       <Work thumbnailUrl={prevUrl} />
-                  </div>
+                  </div>*/}
 
                   <div className="slide content-area ">
                       <div className="site-main scene_element2 scene_element--fadein2">
                         <Work thumbnailUrl={url} workTitle={name} />
                       </div>
                   </div>
-                  
+{/*                  
                   <div className="slide jag-next-post ">
                       <Work thumbnailUrl={nextUrl}  />
                   </div>
-
+*/}
 
               </div>
           </div>
