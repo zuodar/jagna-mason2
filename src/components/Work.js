fb04eb2bc5e944ff99f4d9c7d15fb33cddab2c4b
zuodar <michal.wlodarek@gmail.com>
2019-06-20 19:09:04 +0200

    Data from json file from custom wordpress endpoint

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import Drawer from './Drawer';
- 
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
 const divStyle = {
 	left: '0',
 	right: '0',
@@ -21,34 +22,36 @@ const Work = ( props ) => {
   	console.log( 'props.infoActive', props.infoActive )
 
 	return (
-    	<div>
-
-	    	<div className="jag-info-btn2"> 	
-				<div
-					className="jag-info-btn__txt2"
-					onClick={ props.showInfoDrawer }> Info </div> 				
-			</div>
-
- 			<div className="jag-single-title">
-           		 { props.workTitle}  
-        	</div> 
-
-	    	<div className="jag-image jag-img jag-image1 jag-img-active "> 
-		    	<div className="post-thumbnail">
-			    	<img className="attachment-post-thumbnail size-post-thumbnail wp-post-image"  
-			    	src={ props.thumbnailUrl}
-			    	/> 
-		    	</div> 
-	    	</div>
-
-			<div>
-				{description }
-			</div>
 
 
+            <TransitionGroup className="card-container">
+                <CSSTransition
+                    key={ props.postID }
+                    timeout={ 1000 }
+                    classNames="slide" >
+
+			    	<div className="card"> 
+				    	<div className="jag-info-btn2"> 	
+						{/* <div className="jag-info-btn__txt2"
+							 onClick={ props.showInfoDrawer }> Info </div> 	*/}	
+						</div>
+			 			<div className="jag-single-title"> { props.postTitle} </div> 
+				    	<div className="jag-image jag-img jag-image1 jag-img-active "> 
+					    	<div className="post-thumbnail">
+						    	<img className="attachment-post-thumbnail size-post-thumbnail wp-post-image"  
+						    	src={ props.thumbnailUrl}
+						    	/> 
+					    	</div> 
+				    	</div>
+						<div> {description } </div>
+			    	</div>
 
 
-    	</div>
+                </CSSTransition>
+            </TransitionGroup>
+
+
+
 	)
 }
 
--- src/components/Works.js
+++ src/components/Works.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Work from './Work';
-import data from '../data';
+// import data from '../data';
 import dataJson from '../data.json';
+import { CSSTransition } from "react-transition-group";
 
 const centered = {
 	left: '0',
@@ -12,25 +13,30 @@ const centered = {
 class Works extends Component {
   state = {
     id:0,
-    infoActive: false
-  };
+    infoActive: false,
+    appearHome: true,
+    dataJson:  "http://rawsaw.co/jagna1984/wp-json/sections/v1/post" 
+  }
+
+  toggleAppear = () => {
+    this.setState({
+      appearHome: !this.state.appearHome
+    })
+  }
 
   incrementItem = () => {
-    const dupa = data[0];
-
-      if ( this.state.id + 1 < data.length) {
-        this.setState({ id: this.state.id + 1 });  
-      //  console.log(name);
+      if ( this.state.id + 1 < dataJson.length) {
+        this.setState({ id: this.state.id + 1 });
       } else {
         this.setState({ id: 0 }); 
       }
-  };
+  }
 
   decrementItem = () => {
       if ( this.state.id > 0 ) {
-        this.setState({ id: this.state.id - 1 });  
+        this.setState({ id: this.state.id - 1 });
       } else {
-        this.setState({ id: data.length - 1 }); 
+        this.setState({ id: dataJson.length - 1 });
       }    
   }
 
@@ -42,33 +48,58 @@ class Works extends Component {
 
 	render() {
     const stateId = this.state.id;
-    const url = data[stateId].thumbnail;
-    const name = data[stateId].name;
-    const description = data[stateId].description;
-    const infoActive = data[stateId].infoActive;
+    const url = dataJson[stateId].acf.cst_feat_img.sizes.large;
+    const name = dataJson[stateId].post_title;
+    const ID = dataJson[stateId].ID;
+    const description = dataJson[stateId].description;
+    const infoActive = dataJson[stateId].infoActive;
 
     return (
       <div id="slider" className="jagSwipe" style={centered}>
           <div className="wrapper">
               <div id="slides" className="slides">
 
-
                   <div className="slide content-area ">
                       <div className="site-main scene_element2 scene_element--fadein2">
-                        <Work 
-                          thumbnailUrl={url} 
-                          workTitle={name} 
-                          description={description} 
-                          infoActive={ this.state.infoActive } 
-                          showInfoDrawer={ this.showInfoDrawer } />
+
+                        <CSSTransition
+                          in={this.state.appearHome}
+                          appear={true} 
+                          timeout={ 350}
+                          classNames="fade" >
+                          <Work 
+                            thumbnailUrl={url} 
+                            postTitle={name} 
+                            postID={ID} 
+                            description={description} 
+                            infoActive={ this.state.infoActive } 
+                            showInfoDrawer={ this.showInfoDrawer } />
+                        </CSSTransition>
+
                       </div>
                   </div>
 
-
               </div>
           </div>
-          <button onClick={this.decrementItem}> Prev </button>
-          <button onClick={this.incrementItem}> Next </button>
+
+ 
+
+
+          <button 
+            className="jag-btn-prev-post" 
+            onClick={ () => this.state.toggleAppear(),
+            this.decrementItem }>
+            Prev 
+          </button>
+          
+          <button 
+            className="jag-btn-next-post" 
+            onClick={ () => this.state.toggleAppear(),
+            this.incrementItem }>
+            Next 
+          </button>
+
+
       </div>
 	    )
 	}
